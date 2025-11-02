fd80d5184d6063b3e790f99c5b94d36640a18035
IgorJFS <igormpaz@gmail.com>
2025-11-02 20:28:11 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,23 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   integrations: [vue(), mdx(), sitemap()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    css: {
+      transformer: 'lightningcss'
+    }
   }
 });
